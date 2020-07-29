b2ae54f0e13e19ad0930c5a11b162f6bd59e16ae
laurinha-ssa <53535001+laurinha-ssa@users.noreply.github.com>
2020-07-29 18:53:08 -0300

    Update best.R

--- best.R
+++ best.R
@@ -16,14 +16,14 @@ best <- function(state, outcome) {
   
   ## analisa se o estado existe e parte para verificar os outcomes
   if(x<1){
-    stop("state inexistente")  
+    stop("invalid state")  
   }
-  if(x>=1){
+  else if(x>=1){
     linha = 0
     num = 0
     if(outcome == "heart attack"){
       ##cria uma lista com os valores de mortes para heart attack
-      listado <- as.numeric(dados[, 15])
+      listado <- as.numeric(dados[, 11])
       ## transforma NA em 0
       for(n in listado){
         num=num+1
@@ -36,7 +36,7 @@ best <- function(state, outcome) {
       tabela2 <- tabela[order(tabela[,3], tabela[,1], decreasing=c(FALSE, TRUE)), ]
       for(j in tabela2[,2]){
         linha=linha+1
-        if((tabela2[linha,3] >= 1) && (j == state)){
+        if((tabela2[linha,3] > 0) && (j == state)){
           stop <- TRUE
           nome <- tabela2[linha,1]
           break 
@@ -51,7 +51,7 @@ best <- function(state, outcome) {
     }
     if(outcome == "heart failure"){
       ##cria uma lista com os valores de mortes para heart failure
-      listado <- as.numeric(dados[, 21])
+      listado <- as.numeric(dados[, 17])
       ## transforma NA em 0
       for(n in listado){
         num=num+1
@@ -64,7 +64,7 @@ best <- function(state, outcome) {
       tabela2 <- tabela[order(tabela[,3], tabela[,1], decreasing=c(FALSE, TRUE)), ]
       for(j in tabela2[,2]){
         linha=linha+1
-        if((tabela2[linha,3] >= 1) && (j == state)){
+        if((tabela2[linha,3] > 0) && (j == state)){
           stop <- TRUE
           nome <- tabela2[linha,1]
           break 
@@ -79,7 +79,7 @@ best <- function(state, outcome) {
     }
     if(outcome == "pneumonia"){
       ##cria uma lista com os valores de mortes para pneumonia
-      listado <- as.numeric(dados[, 27])
+      listado <- as.numeric(dados[, 23])
       ## transforma NA em 0
       for(n in listado){
         num=num+1
@@ -92,7 +92,7 @@ best <- function(state, outcome) {
       tabela2 <- tabela[order(tabela[,3], tabela[,1], decreasing=c(FALSE, TRUE)), ]
       for(j in tabela2[,2]){
         linha=linha+1
-        if((tabela2[linha,3] >= 1) && (j == state)){
+        if((tabela2[linha,3] > 0) && (j == state)){
           stop <- TRUE
           nome <- tabela2[linha,1]
           break 
@@ -104,10 +104,10 @@ best <- function(state, outcome) {
           next
         }
       }
-      }
     }
     else{
-      stop("outcome inexistente")
+      stop("invalid outcome")
+    }
     }
   nome
   }
